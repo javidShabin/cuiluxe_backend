e0f4db04e203a3a9b0216f7428d31b94262d226f
javid shabin <javid.main.act@gmail.com>
2025-11-10 00:13:26 +0530

    make change

--- src/api.js
+++ src/api.js
@@ -3,6 +3,7 @@ import userRoutes from "./modules/authentication/auth.routes.js"
 import productRoutes from "./modules/products/product.routes.js";
 import cartRoutes from "./modules/cart/cart.routes.js";
 import profileRoutes from "./modules/profile/profile.routes.js"
+import siteContentRoutes from "./modules/siteContent/siteContent.routes.js";
 
 const router = Router();
 
@@ -10,6 +11,7 @@ router.use("/user", userRoutes)
 router.use("/product", productRoutes);
 router.use("/cart", cartRoutes);
 router.use("/profile", profileRoutes)
+router.use("/site-content", siteContentRoutes);
 
 
 export default router;
